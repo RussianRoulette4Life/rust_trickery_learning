82eba6c85db0cf85f1fc90cff62bfd1031b5f088
Ruslan <petrushin-ruslanrus@yandex.ru>
2022-08-25 16:14:31 +0300

    i am going insane?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct Country {
+    official_name: String,
+    shorthand_name: String,
+    current_leader: String,
+    type_of_govern: String,
+    last_three_leaders: String,
+    area: u64,
+    population: u64,
+    gdp: u64,
+    debt: i64,
+}
 fn main() {
-    println!("Hello world!");
+    
 }
