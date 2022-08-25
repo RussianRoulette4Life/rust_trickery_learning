8c326625b1c1152cd0ae523397cf310534220fe4
Ruslan <petrushin-ruslanrus@yandex.ru>
2022-08-25 14:33:46 +0300

    startin somethin new

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+enum testEnum {
+    test1((i32,i32,i32)),
+    test2(String),
 }
-impl Rectangle {
-    fn calc_area(&self) -> u32 {
-        self.width * self.height
-    }
-    fn can_hold(&self, rectangle_compare: &Rectangle) -> bool{
-        self.height > rectangle_compare.height && self.width > rectangle_compare.width
-    }
-}
-fn main () {
-    let rect1: Rectangle = Rectangle {
-        width: 50,
-        height: 30,
-    };
-    let rect2 = Rectangle {
-        width: 40,
-        height:20,
-    };
-    println!("area thru the method... method: {}", rect1.calc_area());
-    println!("u can fit rect2 in rect1, right? {}. what about the other way around? {}. sure",rect1.can_hold(&rect2), rect2.can_hold(&rect1) );
+fn main(){
+    let tuple:(i32,i32,i32) = (1,7,2);
+    let tesst = testEnum::test1(tuple);
+    println!("{:?}", tesst);
 }
