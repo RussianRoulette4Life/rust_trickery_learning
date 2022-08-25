19b106413c01b8ef42b72d8ac6671d6585554adc
Ruslan <petrushin-ruslanrus@yandex.ru>
2022-08-25 11:42:46 +0300

    leanring structs, perty easy

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,25 @@
-use std::io;
-
-
-fn main(){
-	println!("He-hey! More rust trickery! This time, the program recieves a sentence from YOU, and gives you an output with a lot of cool stuff!");
-	loop {
-		println!("Enter a sentence: ");
-		// creates a mutatable string (it has to be this wayy)
-		let mut sentence: String = String::new();
-		io::stdin()
-			.read_line(&mut sentence)
-			.expect("what did you do???");
-		// its a pain to work with an untrimmed string
-		let trimmed_string = sentence.trim().to_string();
-		// this kind of assignment of values is convenient asf
-		let (amount_of_words, index_last) = count_and_disect_words(&trimmed_string);
-		// print everything outte
-		println!("The original string: {}", trimmed_string);
-		println!("the goofy stuff - words: {}, last character num: i dunno, this variable is redundant, last index: {}",amount_of_words, index_last) 
-	}
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
-// it HAS to take a reference, not a chance at the original
-fn count_and_disect_words (string_sentence: &String) -> (i32, i32){
-	let mut amount_of_words = 0;
-	let mut index_last = 0;
-	// made to prevent 2 spaces count as a separate word
-	let mut last_character_index = 0;
-	// for fun :)
-	if string_sentence == "" {
-		println!("why?")
-	}
-	// prints ALL characters
-	for character in string_sentence.chars(){
-		println!("{}", character)
-	}
-	let mut detected_word: String = String::new();
-	// word detection, that's it
-	for (i, character) in string_sentence.chars().into_iter().enumerate() {
-		if (character != ' ') && (character != '\n') {
-			detected_word.push(character);
-		}
-		last_character_index = if (character != ' ') && (character != '\n') && (character != '-') {
-			i
-		} else {
-			last_character_index
-		};
-        if (character == ' ') && (i <= last_character_index + 1) {
-			amount_of_words = amount_of_words + 1;
-			println!("{}. {}",amount_of_words, detected_word);
-			detected_word = String::new();
-		}
-		index_last = i;
+impl Rectangle {
+    fn calc_area(&self) -> u32 {
+        self.width * self.height
     }
-	// print what cant return, return what can
-	println!("{}. {}",&amount_of_words + 1, &detected_word);
-	(amount_of_words+1, index_last.try_into().unwrap()) 
-	}
+    fn can_hold(&self, rectangle_compare: &Rectangle) -> bool{
+        self.height > rectangle_compare.height && self.width > rectangle_compare.width
+    }
+}
+fn main () {
+    let rect1: Rectangle = Rectangle {
+        width: 50,
+        height: 30,
+    };
+    let rect2 = Rectangle {
+        width: 40,
+        height:20,
+    };
+    println!("area thru the method... method: {}", rect1.calc_area());
+    println!("u can fit rect2 in rect1, right? {}. what about the other way around? {}. sure",rect1.can_hold(&rect2), rect2.can_hold(&rect1) );
+}
