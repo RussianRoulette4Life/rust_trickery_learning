e73b9370714b50ec17a889f958060ff0df230496
Ruslan <petrushin-ruslanrus@yandex.ru>
2022-08-25 14:35:01 +0300

    yes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-#[derive(Debug)]
-enum testEnum {
-    test1((i32,i32,i32)),
-    test2(String),
-}
-fn main(){
-    let tuple:(i32,i32,i32) = (1,7,2);
-    let tesst = testEnum::test1(tuple);
-    println!("{:?}", tesst);
+fn main() {
+    println!("Hello world!");
 }
