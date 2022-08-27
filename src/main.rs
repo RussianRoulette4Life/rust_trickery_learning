8ed6ad84bb2879e9b67dd298faba38f21fd951da
Ruslan <petrushin-ruslanrus@yandex.ru>
2022-08-27 20:59:46 +0300

    program becomes sentient

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,16 @@ mod word_detection;
 use crate::word_detection::count_and_disect_words;
 struct Country {
     official_name: String,
-    shorthand_name: String,
-    current_leader: String,
-    type_of_govern: String,
-    last_three_leaders: String,
-    area: u64,
-    population: u64,
-    gdp: u64,
-    inner_debt: i64,
-    outer_debt: i64,
+    rank_by_population: String,
+    continent: String,
+    population: String,
+    percent_pop_of_world: String,
+    last_pop_count_date: String,
+    means_of_pop_count: String,
 }
 fn main() {
     // Create a path to the desired file
-    let path = Path::new("/home/ruslan/allMyProjects/rust_trickery_learning/src/index.txt");
+    let path = Path::new("/home/ruslan/allMyProjects/rust_trickery_learning/src/index_full.txt");
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -30,14 +27,18 @@ fn main() {
     let mut s = String::new();
     match file.read_to_string(&mut s) {
         Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => print!("file parsed successfully"),
+        Ok(_) => print!(""),
     }
-    let parsed_info = String::from((parse_html(&s).replace("\n", " ")).trim());
-    let amount_of_words = (count_and_disect_words(&parsed_info)).2;
-    println!("{:#?}", amount_of_words);
-    // `file` goes out of scope, and the "hello.txt" file gets closed
+    let amount_of_iterations = count_tr_tags(&s);
+    println!("{amount_of_iterations}");
+    let html_to_parse: String = return_html_up_to_a_tag(&s, 2);
+    let vector: Vec<String> = parse_html(&html_to_parse);
+    println!("{:#?}", vector)
+    // let parsed_info = (parse_html(&s, false).replace("\n", " ")).0;
+    // let parsed_info_finalised = String::from(parsed_info).trim();
+    // let vector_of_parsed_words = (count_and_disect_words(&parsed_info_finalised)).2;
 }
-fn parse_html(html: &String) -> String{
+fn parse_html(html: &String) -> Vec<String>{
     let mut start_iter:bool=false;
     let mut tags:String = String::new();
     let mut string_of_text = String::new();
@@ -53,16 +54,63 @@ fn parse_html(html: &String) -> String{
         if character == '>'{
             start_iter = false;
             if tags == "</tr>"{
-                return string_of_text   
+                let string_final:String = String::from((string_of_text.replace('\n', " ")).trim());
+                return count_and_disect_words(&string_final).2  
             } else {
                 tags = String::new();
             }
         }
     }
-    String::from("sorreh, found nothin")
+    vec![]
 }
-//fn search_thru_tags(string_html: &String) -> String {
-//    for character in string_html.chars(){
-//    
-//    }
-//}
+
+fn count_tr_tags (html: &String) -> u32 {
+    let mut start_iter:bool = false;
+    let mut tags = String::new();
+    let mut num_of_tags:u32 = 0; 
+    for character in html.chars(){
+        if character == '<'{
+                start_iter = true;
+        }
+        if start_iter {
+            tags.push(character);
+        }
+        if (character == '>') {
+            if tags == "</tr>" {
+                num_of_tags += 1;
+            }
+            tags = String::new()
+        } 
+    }
+    return num_of_tags
+}
+fn return_html_up_to_a_tag(html:&String, tag_number:u32) -> String {
+    let mut new_html = String::new();
+    let mut start_iter:bool=false;
+    let mut tags:String = String::new();
+    let mut string_of_text = String::new();
+    let mut num_of_tags: u32 = 0;
+    for character in html.chars(){
+        if character == '<'{
+                start_iter = true;
+        }
+        if start_iter {
+            tags.push(character);
+        }
+        string_of_text.push(character);
+        if character == '>'{
+            start_iter = false;
+            if tags == "</tr>"{
+                num_of_tags+=1;
+                if tag_number == num_of_tags {
+                    return string_of_text
+                } else {
+                    string_of_text = String::new();
+                }
+            } else {
+                tags = String::new();
+            }
+        }
+    }
+    String::from("must've been an error")
+}
--- src/word_detection/mod.rs
+++ src/word_detection/mod.rs
@@ -9,9 +9,9 @@ pub fn count_and_disect_words (string_sentence: &String) -> (i32, i32, Vec<Strin
 		println!("why?")
 	}
 	// prints ALL characters
-	for character in string_sentence.chars(){
-		println!("{}", character)
-	}
+	//for character in string_sentence.chars(){
+	//	println!("{}", character)
+	//}
 	let mut detected_word: String = String::new();
 	// word detection, that's it
 	for (i, character) in string_sentence.chars().into_iter().enumerate() {
@@ -25,7 +25,7 @@ pub fn count_and_disect_words (string_sentence: &String) -> (i32, i32, Vec<Strin
 		};
         if (character == ' ') && (i <= last_character_index + 1) {
 			amount_of_words = amount_of_words + 1;
-			println!("{}. {}",amount_of_words, detected_word);
+			//println!("{}. {}",amount_of_words, detected_word);
             list_of_words.push(detected_word);
 			detected_word = String::new();
 		}
@@ -33,7 +33,7 @@ pub fn count_and_disect_words (string_sentence: &String) -> (i32, i32, Vec<Strin
     }
 	// print what cant return, return what can
 
-    println!("{}. {}",&amount_of_words + 1, &detected_word);
+    //println!("{}. {}",&amount_of_words + 1, &detected_word);
 
     list_of_words.push(detected_word);
 
